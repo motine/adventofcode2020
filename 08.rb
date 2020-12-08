a8cd323b857b6d36e4f8f625150ee3f39424e997
Tom Rothe <info@tomrothe.de>
2020-12-08 20:30:48 +0100

    rework 08a

--- 08.rb
+++ 08.rb
@@ -1,72 +1,104 @@
 module Instruction
-  class RanBefore < StandardError; end
+  module_function
 
-  class Base
-    def initialize(program, param)
-      @program, @param = program, param
-      @ran_before = false
-    end
-
-    def run(increment_counter: true)
-      @program.counter += 1 if increment_counter
-
-      raise RanBefore if @ran_before
-      @ran_before = true
-    end
+  def run(operation_name, program, *params)
+    send(operation_name, program, *params)
   end
 
-  class Nop < Base
-    def run
-      super
-    end
+  def nop(program, param)
   end
 
-  class Acc < Base
-    def run
-      super
-      @program.accumulator += @param.to_i
-    end
+  def acc(program, param)
+    program.accumulator += param.to_i
   end
 
-  class Jmp < Base
-    def run
-      super(increment_counter: false)
-      @program.counter += @param.to_i
-    end
+  def jmp(program, param)
+    program.counter += param.to_i
   end
 end
 
 class Program
+  class RanBefore < StandardError; end
+  class Unchanged < StandardError; end
+
   # may be modified by Instructions
   attr_accessor :accumulator
   attr_accessor :counter
 
   def initialize
-    @instructions = []
+    @instructions = [] # [ ['ope', ['param1', 'param2'], false] ]
+    @instruction_indexes_run_before = []
     @accumulator = 0
     @counter = 0
   end
 
+  # def initialize_copy(original_program)
+  #   @instructions = original_program.instance_variable_get(:@instructions).collect { |instruction| instruction.dup }
+  #   @accumulator = 0
+  #   @counter = 0
+  # end
+
+  def instruction_count
+    @instructions.count
+  end
+
   def run
     loop do
-      @instructions[@counter].run
+      raise RanBefore if @instruction_indexes_run_before.include?(@counter)
+      @instruction_indexes_run_before << @counter
+
+      operation, params = @instructions[@counter]
+
+      counter_before = @counter
+      Instruction.run(operation, self, *params)
+      @counter += 1 if counter_before == @counter # we only move forward if there was no change to the counter
     end
   end
 
   def parse(lines)
     @instructions = lines.collect do |line|
       operation, *params = line.split(' ')
-      operation_class_name = "#{operation[0].upcase}#{operation[1..-1]}" # upcase first letter (poor mans titlize)
-      operation_class = Instruction.const_get(operation_class_name)
-      operation_class.new(self, *params)
+      [operation, params]
     end
   end
+
+  # def repair(instruction_index)
+  #   instruction = @instructions[instruction_index]
+  #   if instruction.is_a?(Instruction::Nop)
+  #     @instructions[instruction_index] = Instruction::Jmp.new(self, instruction.param)
+  #   elsif instruction.is_a?(Instruction::Jmp)
+  #     @instructions[instruction_index] = Instruction::Nop.new(self, instruction.param)
+  #   else
+  #     raise Unchanged
+  #   end
+  # end
+
+  # def inspect # TODO remove
+  #   @instructions.collect {|instruction| instruction.class.name[-3..-1] }.join(', ')
+  # end
 end
 
 program = Program.new
 program.parse(File.readlines('08.txt'))
+
+# part 1
 begin
-  program.run
-rescue Instruction::RanBefore
-  puts program.accumulator # => 1394
+  program2 = program.dup
+  program2.run
+rescue Program::RanBefore
+  puts "part 1: #{program2.accumulator}" # => 1394
 end
+
+# # part 2
+# (0..program.instruction_count).each do |i|
+#   varied_program = program.dup
+#   # varied_program.repair(i)
+#   varied_program.run
+#   puts "part 2: #{varied_program.accumulator}"
+#   break
+# rescue Program::Unchanged
+#   # ignore unchanged programs
+# rescue Instruction::RanBefore
+#   puts "xxx" + varied_program.accumulator.to_s
+#   # ignore non-terminating program
+# end
