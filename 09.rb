fe271cabf3b09735c71998e20af0bdbe06ccaf1b
Tom Rothe <info@tomrothe.de>
2020-12-09 20:03:37 +0100

    09b

--- 09.rb
+++ 09.rb
@@ -26,12 +26,39 @@ class ValidatingBuffer < Buffer
   end
 end
 
+class Array
+  def start_sequence_with_sum(target)
+    0.upto(self.size-1) do |n|
+      total = self[0..n].sum
+      return self[0..n] if total == target
+      return nil if total > target
+    end
+    return nil
+  end
+end
+
 numbers = File.readlines('09.txt').map { |line| line.to_i }
+
+weak_number = :not_found
 
 buffer = ValidatingBuffer.new
 numbers.each do |number|
   buffer << number
 rescue ValidatingBuffer::InvalidSequence
-  p number
+  weak_number = number
   break
 end
+
+puts weak_number # => 248131121
+
+sequence = numbers.dup
+loop do
+  sum_seq = sequence.start_sequence_with_sum(weak_number)
+  if sum_seq
+    puts sum_seq.min + sum_seq.max
+    break
+  end
+  sequence.shift
+  raise 'no result found' if sequence.empty?
+end
+# => 31580383
